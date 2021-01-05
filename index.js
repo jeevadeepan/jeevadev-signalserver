72e9c2fafe6e815591327a939a99060fde6ba45c
Jeevadeepan <jeevacse89@gmail.com>
2021-01-05 16:17:52 -0500

    removed connect server code

--- index.js
+++ index.js
@@ -2,50 +2,8 @@ const fs = require('fs');
 const https = require('https');
 // const http = require('http');
 const WebSocket = require('ws');
-const bodyParser = require('body-parser');
-
-const express = require('express');
 const cors = require('cors');
-const jsonParser = bodyParser.json();
-
-
-const app = express();
-let clientId;
 app.use(cors());
-
-
-const admin = require("firebase-admin");
-
-admin.initializeApp();
-
-
-// create a route for the app
-app.post('/register', jsonParser, (req, res) => {
-  clientId = req.body.token;
-  res.send('Token registered');
-});
-
-// another route
-app.get('/connect', (req, res) => {
-  const message={
-    "token": clientId,
-    "notification": {
-      "title": "Jeeva dev user",
-      "body": "Incoming call from Jeeva.dev user"
-    },
-    "webpush": {
-      "fcm_options": {
-        "link": "https://jeeva.dev/#/chat"
-      }
-    }
-  };
-
-  admin.messaging().send(message).then(response => {
-  }).catch(error => {
-    console.log('Error sending FCM message: ', error);
-  })
-  res.send('Hello!');
-});
 
 const server = https.createServer({
     cert: fs.readFileSync('/etc/letsencrypt/live/signal.jeeva.dev/cert.pem'),
